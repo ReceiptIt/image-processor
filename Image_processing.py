c34ea20ffab6275428a51a7bc23c6e10d2dcc75e
jm3zhang <jm3zhang@gmail.com>
2019-07-09 22:59:54 -0400

    Add the image cropping portion

--- Image_processing.py
+++ Image_processing.py
@@ -1,18 +1,32 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-og_img = cv.imread('a.png',0)
-# img = cv.medianBlur(img,5)
-ret,text_recognition_image = cv.threshold(og_img,160,255,cv.THRESH_BINARY)  # for text recognition
-edge_detection_image = cv.adaptiveThreshold(og_img,255,cv.ADAPTIVE_THRESH_MEAN_C,
-            cv.THRESH_BINARY,15,2)  # for edge detection
-# th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,
-#             cv.THRESH_BINARY,11,2)
-titles = ['Original Image', 'Global Thresholding (v = 150)',
-            'Adaptive Mean Thresholding']
-images = [og_img, text_recognition_image, edge_detection_image]
 
-for i in range(3):
+# input original receipt image
+og_img = cv.imread('a.png',0)
+
+# binarization
+# for text recognition
+ret,text_recognition_image = cv.threshold(og_img,160,255,cv.THRESH_BINARY)
+
+# Gaussian Blurring
+blur = cv.GaussianBlur(og_img,(5,5),0)
+
+# Canny edge detection 
+edges = cv.Canny(blur, 100, 200)
+# Crop image based on Canny edge detection 
+edge_pts = np.argwhere(edges>0)
+y1,x1 = edge_pts.min(axis=0)
+y2,x2 = edge_pts.max(axis=0)
+crop_image = text_recognition_image[y1:y2, x1:x2]
+
+# new image title 
+titles = ['Original Image', 'Gaussian Blurring Image', 'Global Thresholding (v = 160)', 'Cropped Image']
+# image list 
+images = [og_img, blur, text_recognition_image, crop_image]
+
+# plot
+for i in range(4):
     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
